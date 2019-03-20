4d9f822704d4e9b0c4cd6d012b5bd5d41352c601
Benjamin Cooper <bencooper222@gmail.com>
2019-03-20 17:05:44 -0500

    Description of code stuff to do

--- week1/meeting/fun.js
+++ week1/meeting/fun.js
@@ -2,6 +2,8 @@
 
 // now let's go over let/const, if statements, for loops, while loops and all the basic language features
 
-// unique js things like triple equals, everything is an object, function callbacks etc
+// unique js things everything is an object, function callbacks etc
 
-// quick think on fetch()
+// quick think on fetch() with the weather api.
+
+// now a case that brings up why ts is necessary.
