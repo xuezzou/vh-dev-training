82496795e0f0841e80f0fa1f9421efd120a19d32
Benjamin Cooper <bencooper222@gmail.com>
2019-03-21 17:12:26 -0500

    comment out to stop contaminating shit

--- week1/meeting/fun.js
+++ week1/meeting/fun.js
@@ -8,69 +8,69 @@ let c = false;
 
 // let's talk about why var sucks
 
-// we can (and should, whenever possible) enforce constness
-const d = 3;
-try {
-	d = 7;
-} catch (err) {
-	console.log(err);
-}
+// // we can (and should, whenever possible) enforce constness
+// const d = 3;
+// try {
+// 	d = 7;
+// } catch (err) {
+// 	console.log(err);
+// }
 
-// all your normal language features are here
+// // all your normal language features are here
 
-for (let i = 0; i < a; i++) {
-	console.log('BOO');
-}
+// for (let i = 0; i < a; i++) {
+// 	console.log('BOO');
+// }
 
-// notice that we have an exponent operator!
-while (a < d ** 2) {
-	a **= 3;
-	console.log(a);
-}
+// // notice that we have an exponent operator!
+// while (a < d ** 2) {
+// 	a **= 3;
+// 	console.log(a);
+// }
 
-// NOTE: in a lot of cases, it's bad form to use these loop structures. Your HW will cover some other ones.
+// // NOTE: in a lot of cases, it's bad form to use these loop structures. Your HW will cover some other ones.
 
-// there's also functions. They sometimes look like this:
+// // there's also functions. They sometimes look like this:
 
-function hello(param) {
-	console.log(param);
-}
+// function hello(param) {
+// 	console.log(param);
+// }
 
-// or this
-const hola = function(param) {
-	console.log(param);
-};
+// // or this
+// const hola = function(param) {
+// 	console.log(param);
+// };
 
-// or this
-const bonjour = param => {
-	console.log(param);
-};
+// // or this
+// const bonjour = param => {
+// 	console.log(param);
+// };
 
-// or this
-const ciao = (param1, param2) => {
-	console.log(param1 + param2);
-};
+// // or this
+// const ciao = (param1, param2) => {
+// 	console.log(param1 + param2);
+// };
 
-// there's also switch statements, imports/exports and all the usual stuff. We'll worry about that stuff later.
+// // there's also switch statements, imports/exports and all the usual stuff. We'll worry about that stuff later.
 
-if (a == 5) {
-	console.log('a is 5');
-}
+// if (a == 5) {
+// 	console.log('a is 5');
+// }
 
-// you might have noticed that there's a little red line under the == in the previous statement. Why?
+// // you might have noticed that there's a little red line under the == in the previous statement. Why?
 
-// Another JS thing: note that type is NOT enforced soo...
-console.log(a); // sidenote: logging!
-a = "it's a string now!";
-console.log(a);
+// // Another JS thing: note that type is NOT enforced soo...
+// console.log(a); // sidenote: logging!
+// a = "it's a string now!";
+// console.log(a);
 
-// fortunately, we have typescript. Let's switch!
-// notice the new errors. usually typescript can infer type but we can explicitly declare it if we like.
-// we have to do that for the function parameters earlier. Let's try it.
+// // fortunately, we have typescript. Let's switch!
+// // notice the new errors. usually typescript can infer type but we can explicitly declare it if we like.
+// // we have to do that for the function parameters earlier. Let's try it.
 
-// unique js things everything is an object, function callbacks etc
-// let's log a function from earlier:
-console.log(ciao); // functions are just variables
-console.log(console); // let's get crazy
+// // unique js things everything is an object, function callbacks etc
+// // let's log a function from earlier:
+// console.log(ciao); // functions are just variables
+// console.log(console); // let's get crazy
 
-// now a case that brings up why ts is necessary.
+// // now a case that brings up why ts is necessary.
