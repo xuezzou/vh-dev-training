e9f30882a354c16a7ea48ee5a7f7f35e1c34d458
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2019-04-04 19:04:34 -0500

    Set up Koa example

--- week3/meeting/index.ts
+++ week3/meeting/index.ts
@@ -5,6 +5,7 @@
 
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
+import fetch from 'node-fetch';
 
 const PORT = 3000;
 
@@ -20,6 +21,33 @@ router.get('/', ctx => {
 router.get('/log', ctx => {
 	ctx.body = 'Check your console ;)';
 	console.log(`Logged at ${new Date()}`);
+});
+
+// Okay, but what can we really do?
+router.get('/weather', async ctx => {
+	// Let's assume we get a request with a "zip"; we'll grab the query
+	const { query } = ctx.request;
+
+	// Next, let's use this ZIP code to hit the OpenStreetMap API
+	const geoDataJson: any = await (await fetch(
+		`https://nominatim.openstreetmap.org/?format=json&q=${query.zip}`
+	)).json();
+
+	const latitude: any = geoDataJson[0].lat;
+	const longitude: any = geoDataJson[1].lon;
+
+	// Great! Now we can get some weather data.
+	const weatherDataJson: any = await (await fetch(
+		`https://api.darksky.net/forecast/436b7ccabd9a90440c7fed694e71d54f/${latitude},${longitude}`
+	)).json();
+
+	ctx.body = {
+		lat: latitude,
+		lon: longitude,
+		weather: `It's ${weatherDataJson.currently.summary} and it's ${
+			weatherDataJson.currently.temperature
+		} degrees.`,
+	};
 });
 
 // Set up our app to use our defined routes
